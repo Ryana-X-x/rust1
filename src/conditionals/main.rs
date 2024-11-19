97b138f7934638b4927f0fd0570a198e21fdc314
Ryana-X-x <aryansehgal3212@gmail.com>
2024-11-20 02:19:14 +0530

    Learning Data Types and Referential Types

---
+++ src/conditionals/main.rs
@@ -0,0 +1,23 @@
+// ********* Conditionals **********
+
+// pub fn main() {
+//     let x = 99 ;
+//     let is_even = is_even(x) ;
+//     if is_even {
+//         println!("{} is even", x) ;
+//     } else {
+//         println!("{} is odd", x) ;
+//     }
+// }
+
+// pub fn is_even(x:i32) -> bool {
+//     return x % 2 == 0 ;
+// }
+
+// *********** Loops ************
+
+pub fn main() {
+    for i in 0..11 {
+        print!("i: {}, ",i) ;
+    }
+}
